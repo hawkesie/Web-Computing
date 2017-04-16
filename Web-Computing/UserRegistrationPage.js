1fe9b5d4d2af4f75a7566b0e3562f918dec9f89c
hawkesie <jordanhawkes93@gmail.com>
2017-04-16 21:22:42 +1000

    Add files via upload

--- Web-Computing/UserRegistrationPage.js
+++ Web-Computing/UserRegistrationPage.js
@@ -2,6 +2,8 @@ function checkValues(){
 	var name = document.getElementById('name').value;
 	var password = document.getElementById('password').value;
 	var confirmPassword= document.getElementById('confirmPassword').value;
+	var email= document.getElementById('email').value;
+	var postcode= document.getElementById('postcode').value;
 	if(name==""){
 		window.alert("Name field is empty!");
 		return false;
@@ -15,4 +17,19 @@ function checkValues(){
 		return false;
 	}
 
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+
+    var regex = /^[0-9]{4}$/;
+    if(regex.test(postcode)==false){
+    	window.alert("Invalid Postcode");
+    	return false;
+    }
+
+
 }
+
